230f22b0a61230d322980a6d76b7c42066c95ec0
unknown <willy98172@gmail.com>
2022-12-01 16:50:20 +0800

    Chieh

--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -7,3 +7,42 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+// R2 = R0 * R1
+
+//    int R2 = 0;
+//    if(R0 >= 0)
+//    {
+//        for(i = R1; i < 0; i--)
+//        {
+//            R2 = R2 + R0;
+//        }
+//    }
+//    print(R2);
+
+@2
+M = 0
+
+@0
+D = M
+@LOOP
+D;JGE
+
+(LOOP)
+    @1
+    M = M - 1
+    D = M
+    @END
+    D;JLT
+
+    @0
+    D = M
+
+    @2
+    M = M + D
+@LOOP
+    0;JMP
+
+(END)
+@END
+    0;JMP
+
