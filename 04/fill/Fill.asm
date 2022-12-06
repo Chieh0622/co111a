b313c4b4fe015d990d0ac9d0755ea28256fe259b
unknown <willy98172@gmail.com>
2022-12-06 13:31:14 +0800

    Chieh

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -12,3 +12,88 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+//use keyboard control screen color
+
+//int start = screen.open, end = screen.close;
+//int screen.color = white;
+//while(true)
+//{
+//    if(KBD.press == true)
+//    {
+//        for(i = start, i < end, ++i)
+//        {
+//            screen.color = black;
+//        }
+//    }
+//    else
+//    {
+//        for(i = start, i < end, ++i)
+//        {
+//            screen.color = white;
+//        }
+//    }
+//}
+
+@8192
+D = A
+@END
+M = D
+
+(LOOP)
+    @SCREEN
+    D = A
+    @PIX
+    M = D
+
+    @i
+    M = 0
+
+    @KBD
+    D = M
+
+    @WHITE
+    D;JEQ      
+    @BLACK
+    D;JGT
+
+(WHITE)
+    @END
+    D = M
+    @i
+    D = D - M
+    @LOOP
+    D;JEQ
+
+    @PIX
+    A = M
+    M = 0
+
+    @i
+    M = M + 1
+
+    @PIX
+    M = M + 1
+
+    @WHITE
+    0;JMP
+
+(BLACK)
+    @END
+    D = M
+    @i
+    D = D - M
+    @LOOP
+    D;JEQ   
+    
+    @PIX
+    A = M
+    M = -1
+
+    @i
+    M = M + 1
+
+    @PIX
+    M = M + 1
+
+    @BLACK
+    0;JMP
